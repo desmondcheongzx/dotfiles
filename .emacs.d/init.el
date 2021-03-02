7798cd0e9af6ccae36f0c121a7ecc8ffc8d554d0
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2021-03-01 23:41:43 -0500

    Add set-up for golang

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -555,6 +555,67 @@ invoked from a Python process, it will switch back to the `python-mode' buffer."
 (require 'rust-mode)
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
+
+;;; Golang
+                                        ; pkg go installation
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+(setq exec-path (append '("/home/desmond/go/bin") exec-path))
+(setenv "PATH" (concat "/home/desmond/go/bin:" (getenv "PATH")))
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(defun my-go-mode-hook ()
+  (setq tab-width 2 indent-tabs-mode 1)
+                                        ; eldoc shows the signature of the function at point in the status bar.
+  (go-eldoc-setup)
+  (local-set-key (kbd "M-.") #'godef-jump)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+
+                                        ; extra keybindings from https://github.com/bbatsov/prelude/blob/master/modules/prelude-go.el
+  (let ((map go-mode-map))
+    (define-key map (kbd "C-c a") 'go-test-current-project) ;; current package, really
+    (define-key map (kbd "C-c m") 'go-test-current-file)
+    (define-key map (kbd "C-c .") 'go-test-current-test)
+    (define-key map (kbd "C-c C-c") 'go-run)))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+                                        ; Use projectile-test-project in place of 'compile'; assign whatever key you want.
+(global-set-key [f9] 'projectile-test-project)
+
+                                        ; "projectile" recognizes git repos (etc) as "projects" and changes settings
+                                        ; as you switch between them. 
+(projectile-global-mode 1)
+(require 'go-projectile)
+(go-projectile-tools-add-path)
+
+                                        ; "company" is auto-completion
+(require 'company)
+(require 'go-mode)
+(require 'company-go)
+(add-hook 'go-mode-hook (lambda ()
+                          (company-mode)
+                          (set (make-local-variable 'company-backends) '(company-go))))
+
+                                        ; gotest defines a better set of error regexps for go tests, but it only
+                                        ; enables them when using its own functions. Add them globally for use in
+(require 'compile)
+(require 'gotest)
+(dolist (elt go-test-compilation-error-regexp-alist-alist)
+  (add-to-list 'compilation-error-regexp-alist-alist elt))
+(defun prepend-go-compilation-regexps ()
+  (dolist (elt (reverse go-test-compilation-error-regexp-alist))
+    (add-to-list 'compilation-error-regexp-alist elt t)))
+(add-hook 'go-mode-hook 'prepend-go-compilation-regexps)
                                         ;set the default dictionary
 ;;(setq ispell-dictionary "british")
 
